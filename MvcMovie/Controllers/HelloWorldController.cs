f23e6ee3a3ee9b70739df4ca86c064fdacd46692
zhangbo <zhangbobo1234yeah@126.com>
2018-03-07 23:41:03 +0800

    core-mvc

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name,int numTimes = 1)
+        {
+            //使用HtmlEncoder.Default.Encode防止恶意输入，即(JavaScript)损害应用
+            //return HtmlEncoder.Default.Encode($"Hello {name},ID is: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
